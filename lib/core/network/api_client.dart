e515337b31bfe39e0e6601c9dabd571a51022a80
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-19 08:08:57 +0700

    refactor: update ApiClient constructor

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -3,15 +3,12 @@ import 'package:weather_app/core/config/env.dart';
 
 class ApiClient {
   final Client _client;
-  final String _baseUrl;
-  final String _apiKey;
+  final String _baseUrl = Env.baseUrl;
+  final String _apiKey = Env.apiKey;
 
-  ApiClient({Client? client, String? baseUrl, String? apiKey})
-    : _client = client ?? Client(),
-      _baseUrl = baseUrl ?? Env.baseUrl,
-      _apiKey = apiKey ?? Env.apiKey;
+  ApiClient(this._client);
 
-  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) {
+  Future<Response> get(String path, {Map<String, String>? queryParameters}) {
     final uri = Uri.parse('$_baseUrl/$path').replace(
       queryParameters: {
         'appid': _apiKey,
