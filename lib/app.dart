16897cbd7c35c672bfc02732b05486cf0b0d06d3
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-19 18:05:14 +0700

    feat: use theme controller in MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:weather_app/core/routes/app_router.dart';
+import 'package:weather_app/core/theme/app_theme.dart';
+import 'package:weather_app/shared/controllers/theme_controller.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    final ThemeController controller = Get.find();
+
+    return Obx(() => GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: controller.themeMode.value,
       initialRoute: Routes.home,
       getPages: AppRouter.pages,
-    );
+    ));
   }
 }
