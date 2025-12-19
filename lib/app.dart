f7915218f74fcc7c2995c44aa88093b7b160f90e
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-19 08:04:48 +0700

    feat: add WidgetsBinding in main and use GetMaterialApp in MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:weather_app/core/routes/app_router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.home,
+      getPages: AppRouter.pages,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/app.dart';
+import 'package:weather_app/core/di/dependency_injection.dart';
+import 'package:weather_app/core/utils/permissions.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permissions.requestLocation();
+  await DependencyInjection.init();
   runApp(MyApp());
 }
